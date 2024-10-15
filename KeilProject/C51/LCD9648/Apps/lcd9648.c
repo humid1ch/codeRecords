7ecf3f649556119a38d1e8eb55b6a09a927cb094
humid1ch <dxyt2002@163.com>
2024-10-15 20:16:24 +0800

    update

--- KeilProject/C51/LCD9648/Apps/lcd9648.c
+++ KeilProject/C51/LCD9648/Apps/lcd9648.c
@@ -10,10 +10,12 @@ sbit SDA = P0 ^ 5; // SPI数据端
 
 void LCD9648_Init() {
     // 硬件复位
+    RST = 1;
+    delayNMs(10);
     RST = 0; // 复位拉低
-    delay10Us();
+    delayNMs(10);
     RST = 1; // 复位拉高
-    delay10Us();
+    delayNMs(10);
 
     CS0 = 1; // 片选(使能)拉高, 空闲
     SCL = 0; // SPI时钟端拉低, 空闲
@@ -23,55 +25,112 @@ void LCD9648_Init() {
     LCD9648_WriteCmd(0xA0); // 段选方向?(决定了内存数据在, 显示器像素点的映射) 0xA0正常方向, 0xA1反方向
     LCD9648_WriteCmd(0x2F); // 0x2F命令, 启动 LCD9648, 不一定显示内容
 
-    LCD9648_WriteCmd(0x26); // 发送0x26 开始对比度设置
+    LCD9648_WriteCmd(0x26); // 发送0x26 开始对比度控制
     LCD9648_WriteCmd(0x81); // 发送0x81 开启设置对比度值设置
     LCD9648_WriteCmd(0x10); // 设置具体对比度 0x10
 
     LCD9648_WriteCmd(0xAF); // 发送0xAF 显示屏开启显示内容
-    LCD9648_WriteCmd(0x40); // 设置滚动行?
 }
-void LCD9648_Clear() {}
+
+void LCD9648_Clear() {
+    u8 i = 0;
+    u8 j = 0;
+    CS0 = 1; // 空闲
+    SCL = 0; // 空闲
+
+    // 滚动方向
+    for (i = 0; i < 9; i++) {
+        LCD9648_WriteCmd(0x40);
+        // 页选
+        LCD9648_WriteCmd(0xB0 + i); // 0xB0 应该是首页地址
+
+        // 列地址设置
+        LCD9648_WriteCmd(0x10); // 列地址, 高位0001
+        LCD9648_WriteCmd(0x00); // 列地址, 低位0000
+
+        // 写入数据时, 下一个写入地址会自增
+        for (j = 0; j < LCD_WIDTH; j++)
+            LCD9648_WriteData(0x00);
+    }
+}
+
 // 暂时不了解 LCD9648的 极性 和 相性
-void LCD9648_WriteData(u8 u8data) {
+static void LCD9648_WriteData(u8 u8data) {
     u8 i = 0;
-    CS0 = 0; // 片选拉低
-    RS = 0;  // 写数据
 
-    SCL = 0; // 时钟拉低, 空闲(猜的)
+    SCL = 0; // 时钟拉低, 空闲
+    delay10Us();
+
+    CS0 = 0; // 片选拉低
+    delay10Us();
+    RS = 1; // 写数据
     // MSB 先发高位
     for (i = 0; i < 8; i++) {
-        SDA = u8data >> 7;
-        u8data <<= 1;
+        SDA = (u8data & 0x80) ? 1 : 0; // 取高位
+        u8data <<= 1;                  // 去高位
+
         SCL = 1; // 时钟拉高, 发送
         delay10Us();
         SCL = 0; // 时钟拉低, 空闲
         delay10Us();
     }
 
+    SCL = 1; // 时钟拉高, 发送
+    delay10Us();
+    SCL = 0; // 时钟拉低, 空闲
+    delay10Us();
     CS0 = 1; // 片选拉高
 }
 
-void LCD9648_WriteCmd(u8 u8cmd) {
+static void LCD9648_WriteCmd(u8 u8cmd) {
     u8 i = 0;
-    CS0 = 0; // 片选拉低
-    RS = 1;  // 写命令
 
-    SCL = 0; // 时钟拉低, 空闲(猜的)
+    SCL = 0; // 时钟拉低, 空闲
+    delay10Us();
+    CS0 = 0; // 片选拉低
+    delay10Us();
+    RS = 0; // 写命令
+
     // MSB 先发高位
     for (i = 0; i < 8; i++) {
-        SDA = u8cmd >> 7;
-        u8cmd <<= 1;
+        SDA = (u8cmd & 0x80) ? 1 : 0; // 取高位
+        u8cmd <<= 1;                  // 去高位
+
         SCL = 1; // 时钟拉高, 发送
         delay10Us();
         SCL = 0; // 时钟拉低, 空闲
         delay10Us();
     }
 
+    SCL = 1; // 时钟拉高, 发送
+    delay10Us();
+    SCL = 0; // 时钟拉低, 空闲
+    delay10Us();
     CS0 = 1; // 片选拉高
 }
 
-void LCD_SHOW_Char(u8 x, u8 y, char c);
+void LCD_SHOW_Char(u8 x, u8 y, char c) {}
 void LCD_SHOW_String(u8 x, u8 y, u8 len, const char* str);
-void LCD_SHOW_16HZ(u8 x, u8 y, const u8* cn_map);
+void LCD_SHOW_16HZ(u8 x, u8 y, const u8* cnMap);
 void LCD_SHOW_Num(u8 x, u8 y, u32 num, u8 len, u8 mode);
-void LCD_SHOW_Image(u8* p);
+void LCD_SHOW_Image(u8* imageMap) {
+    u8 i = 0;
+    u8 j = 0;
+
+    // 滚动方向
+    for (i = 0; i < 6; i++) {
+        LCD9648_WriteCmd(0x40);
+        // 页选
+        LCD9648_WriteCmd(0xB0 + i); // 0xB0 应该是首页地址
+
+        // 列地址设置
+        LCD9648_WriteCmd(0x10); // 列地址, 高位0001
+        LCD9648_WriteCmd(0x00); // 列地址, 低位0000
+
+        // 写入数据时, 下一个写入地址会自增
+        for (j = 0; j < LCD_WIDTH; j++) {
+            LCD9648_WriteData((*imageMap));
+            imageMap++;
+        }
+    }
+}
--- KeilProject/C51/LCD9648/Users/main.c
+++ KeilProject/C51/LCD9648/Users/main.c
@@ -1,5 +1,20 @@
 #include "reg52.h"
 #include "util.h"
 #include "lcd9648.h"
+#include "pic.h"
 
-void main() {}
+void main() {
+    LCD9648_Init();
+
+    delayNMs(2000);
+    //清屏
+    LCD9648_Clear();
+    delayNMs(2000);
+
+    //显示图片
+    LCD_SHOW_Image(gImage_pic);
+
+    delayNMs(2000);
+    while (1)
+        ;
+}
